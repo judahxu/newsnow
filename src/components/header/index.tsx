90f3eff309ec82c233220d4a9db1a5f2543c9971
xuzhaodong <xuzhaodong@vchaoxi.com>
2024-10-29 16:13:44 +0800

    4

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -62,10 +62,11 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
-            <p>News</p>
-            <p className="mt--1">
-              <span className="color-primary-6">N</span>
-              <span>ow</span>
+            <p>鲸</p>
+            {/* className="mt--1" */}
+            <p>
+              <span className="color-primary-6">闻</span>
+              {/* <span>ow</span> */}
             </p>
           </span>
         </Link>
