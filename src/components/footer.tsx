3e53e807fcf0fb71d796d336484e28ace08d13f1
xuzhaodong <xuzhaodong@vchaoxi.com>
2024-10-29 15:59:53 +0800

    2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,11 @@ import { Author, Homepage } from "@shared/consts"
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a>
+      {/* <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a> */}
       <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
+        <span>鲸闻 © 2024 By </span>
+        <a href="https://coijing.com" target="_blank">
+          河南可爱鲸信息技术有限公司
         </a>
       </span>
     </>
--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -62,7 +62,7 @@ export function Menu() {
             }}
           >
             <ol className="bg-base bg-op-70! backdrop-blur-md p-2 rounded-lg color-base text-base">
-              {loggedIn
+              {/* {loggedIn
                 ? (
                     <li onClick={logout}>
                       <span className="i-ph:sign-out-duotone inline-block" />
@@ -74,9 +74,9 @@ export function Menu() {
                       <span className="i-ph:sign-in-duotone inline-block" />
                       <span>Github 账号登录</span>
                     </li>
-                  )}
+                  )} */}
               <ThemeToggle />
-              <li onClick={() => window.open(Homepage)}>
+              {/* <li onClick={() => window.open(Homepage)}>
                 <span className="i-ph:github-logo-duotone inline-block" />
                 <span>Star on Github </span>
               </li>
@@ -97,7 +97,7 @@ export function Menu() {
                     src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github"
                   />
                 </a>
-              </li>
+              </li> */}
             </ol>
           </motion.div>
         </div>
